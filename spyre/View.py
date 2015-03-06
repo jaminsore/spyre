1696494c892e73d08f6d043faa15d3d8ff777ad1
Benjamin <benjamin.sorenson81@gmail.com>
2015-03-06 11:25:47 -0600

    refactored Vew.View

--- spyre/View.py
+++ spyre/View.py
@@ -11,38 +11,42 @@ ENCODING = 'utf-8'
 class View:
 	def __init__(self):
 		self.ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-		self.JS_PATH = self.ROOT_DIR+'/public/js/'
-		self.CSS_PATH = self.ROOT_DIR+'/public/css/'
+		self.JS_PATH = os.path.join(self.ROOT_DIR, 'public', 'js')
+		self.CSS_PATH = os.path.join(self.ROOT_DIR, 'public', 'css')
 
 	def getHTML(self):
-		file_path = self.ROOT_DIR+'/view.html'
+		file_path = os.path.join(self.ROOT_DIR, 'view.html')
 		f = codecs.open(file_path, 'r', ENCODING)
 		return f.read()
 
 	def getJS(self):
-		self.JS = ""
-		for file in os.listdir(self.JS_PATH):
-			if file.find('.js')>0:
-				file_path = self.JS_PATH+file
-				print(file_path)
-				with codecs.open(file_path, 'r', ENCODING) as f:
-					self.JS += f.read()
-				self.JS += "\n"
-		return self.JS
+		js_files = (os.path.join(self.JS_PATH, f) 
+					for f in os.listdir(self.JS_PATH) if f.endswith('.js'))
 
+		return self._read_files(js_files)
+
+	def _read_files(self, filename_iter, line_end='\n'):
+		try:
+			open_files = [codecs.open(f, 'r', ENCODING) for f in filename_iter]
+			contents = line_end.join(f.read() for f in open_files)
+		finally:
+			for open_file in open_files:
+				open_file.close()
+		return contents
+
+		
 	def getCSS(self):
-		self.CSS = ""
-		for file in os.listdir(self.CSS_PATH):
-			if file.find('.css')>0:
-				file_path = self.CSS_PATH+file
-				with codecs.open(file_path, 'r', ENCODING) as f:
-					self.CSS += f.read()
-				self.CSS += "\n"
-		return self.CSS
+		css_files = (os.path.join(self.CSS_PATH, f) 
+					 for f in os.listdir(self.CSS_PATH) if f.endswith('.css'))
+
+		return self._read_files(css_files)
 
 	def getSpinningWheel(self):
 		buffer = io.BytesIO()
-		f = open(self.ROOT_DIR+'/public/images/loading_wheel.gif','rb')
-		buffer.write(f.read())
-		f.close()
-		return(buffer)
+		path = os.path.join(self.ROOT_DIR, 'public', 'images', 
+							'loading_wheel.gif')
+		with open(path, 'rb') as f:
+			buffer.writelines(f)
+		return buffer
+	
+	
